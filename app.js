77250ee58479037c2e1572aea8192e8e27a9ce80
rahimiairfhussain <arifhussain.rahimi84@gmail.com>
2024-02-08 09:38:08 +0430

    add functionaltiy generatepass js

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const generateBtn = document.getElementById("generateBtn");
 const showPara = document.getElementById("content");
+
 generateBtn.addEventListener("click", () => {
-  showPara.innerHTML = "arif Hussain";
+  // showPara.innerHTML = "arif Hussain";
+  let pass = "";
+  let str =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz0123456789@#$";
+  for (let i = 1; i <= 8; i++) {
+    let char = Math.floor(Math.random() * str.length + 1);
+    pass += str.charAt(char);
+  }
+  showPara.innerHTML = pass;
 });
